3857b97d4f90b1aa7e56a61cffb93bc179a0c56d
RenzoAssenza <88751475+RenzoAssenza@users.noreply.github.com>
2022-05-17 21:46:53 -0300

    Service Ingresos

--- CtrlPlus/src/main/java/com/ctrlplus/controlplus/servicios/IngresoServicio.java
+++ CtrlPlus/src/main/java/com/ctrlplus/controlplus/servicios/IngresoServicio.java
@@ -2,9 +2,11 @@ package com.ctrlplus.controlplus.servicios;
 
 import com.ctrlplus.controlplus.entidades.Foto;
 import com.ctrlplus.controlplus.entidades.Ingreso;
+import com.ctrlplus.controlplus.errores.ErrorServicio;
 import com.ctrlplus.controlplus.repositorios.IngresoRepositorio;
 import java.util.Date;
-
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -29,10 +31,48 @@ public class IngresoServicio {
         return ingresoRepositorio.save(ingreso);
     }
 
-    public void validar(Double monto, Categoria categoria) throws ErrorServicio {
+    @Transactional(propagation = Propagation.NESTED)
+    public void modificar(String id, Double monto, String descripcion, Foto foto) throws ErrorServicio {
+
+        validar(monto);
+
+        Optional<Ingreso> respuesta = ingresoRepositorio.findById(id);
+        if (respuesta.isPresent()) {
+            Ingreso ingreso = respuesta.get();
+            ingreso.setMonto(monto);
+            ingreso.setFecha(new Date());
+            ingreso.setDescripcion(descripcion);
+            ingreso.setFoto(foto);
+            ingresoRepositorio.save(ingreso);
+
+        } else {
+            throw new ErrorServicio("No existe el gasto que desea modificar.");
+        }
+    }
+
+    @Transactional(propagation = Propagation.NESTED)
+    public void eliminar(String id) throws ErrorServicio {
+
+        Optional<Ingreso> respuesta = ingresoRepositorio.findById(id);
+
+        if (respuesta.isPresent()) {
+            Ingreso ingreso = respuesta.get();
+            ingresoRepositorio.delete(ingreso);
+        } else {
+            throw new ErrorServicio("No existe el gasto que desea eliminar.");
+        }
+    }
+
+    public List<Ingreso> listar() {
+        return (List<Ingreso>) ingresoRepositorio.findAll();
+    }
+    
+    
+    public void validar(Double monto) throws ErrorServicio {
         if (monto == null || monto.toString().isEmpty()) {
             throw new ErrorServicio("Debe ingresar un importe.");
+
         }
 
-    
+    }
 }
