87865af8e7f15ebeebf2f7b444ebb0fe7a6af191
Dylan Pavón <dpavon217@gmail.com>
2022-05-31 20:19:10 -0300

    GastoControlador
    Uniendo el usuario con el Gasto

--- CtrlPlus/src/main/java/com/ctrlplus/controlplus/controladores/GastoControlador.java
+++ CtrlPlus/src/main/java/com/ctrlplus/controlplus/controladores/GastoControlador.java
@@ -1,8 +1,11 @@
 package com.ctrlplus.controlplus.controladores;
 
+import com.ctrlplus.controlplus.entidades.Usuario;
 import com.ctrlplus.controlplus.enums.Categoria;
 import com.ctrlplus.controlplus.errores.ErrorServicio;
 import com.ctrlplus.controlplus.servicios.GastoServicio;
+import com.ctrlplus.controlplus.servicios.UsuarioServicio;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -18,12 +21,24 @@ public class GastoControlador {
 
     @Autowired
     private GastoServicio gastoServicio;
+    @Autowired
+    private UsuarioServicio usuarioServicio;
 
     @PostMapping
-    public String ingresar(ModelMap modelo, @RequestParam() Double monto, @RequestParam(required = false) String descripcion, @RequestParam(required = false) MultipartFile archivo, @RequestParam Categoria categoria) {
-        try {
-            gastoServicio.agregar(monto, categoria, descripcion, archivo);
+    public String ingresar(ModelMap modelo,
+            HttpSession session,
+            @RequestParam() Double monto,
+            @RequestParam(required = false) String descripcion,
+            @RequestParam(required = false) MultipartFile archivo,
+            @RequestParam Categoria categoria) {
 
+        Usuario logeado = (Usuario)session.getAttribute("usuariosession");
+        if (logeado == null) {
+            return "index";
+        }  try {
+            usuarioServicio.agregarGasto(logeado, gastoServicio.agregar(monto, categoria, descripcion, archivo));
+            //preguntar a pilar si esta buena esta forma de vincular usuario con ingreso/registro
+            //o si hacerlo en el servicio del ingreso
             return "index";
         } catch (ErrorServicio e) {
 
@@ -39,7 +54,7 @@ public class GastoControlador {
             return "index";
         }
     }
-    
+
     @PostMapping
     public String modificar(ModelMap modelo, @RequestParam() Double monto, @RequestParam(required = false) String descripcion, @RequestParam(required = false) MultipartFile archivo, @RequestParam() String id, @RequestParam Categoria categoria) {
         try {
@@ -53,7 +68,7 @@ public class GastoControlador {
             if (descripcion != null) {
                 modelo.addAttribute("descripcion", descripcion);
             }
-             if (archivo != null) {
+            if (archivo != null) {
                 modelo.addAttribute("comprobante", archivo);
             } //in God we trust
             modelo.addAttribute("id", id);
@@ -61,11 +76,13 @@ public class GastoControlador {
         }
 
     }
+
     @GetMapping
     public String listar(ModelMap modelo) {
         gastoServicio.listar();
         return "index";// devolver donde se vea
     }
+
     @PostMapping
     public String eliminar(ModelMap modelo, @RequestParam String id) {
 
